3bbf725eb60d25ce4891da61a83cfd2e97e73b7a
jhshakil <shakil11271@gmail.com>
2024-02-05 00:05:04 +0600

    remove and replace image from edge store

--- components/modals/cover-image-modal.tsx
+++ components/modals/cover-image-modal.tsx
@@ -32,6 +32,9 @@ const CoverImageModal = () => {
 
       const res = await edgestore.publicFiles.upload({
         file,
+        options: {
+          replaceTargetUrl: coverImage.url,
+        },
       });
 
       await update({
