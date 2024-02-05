1173dfced199d4daf2674d1264713f028abee9a2
jhshakil <shakil11271@gmail.com>
2024-02-05 22:53:27 +0600

    manage page

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEdgeStore } from "@/lib/edgestore";
 import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
 import { BlockNoteView, useBlockNote } from "@blocknote/react";
 import "@blocknote/react/style.css";
@@ -13,6 +14,16 @@ interface EditorProps {
 
 const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
   const { resolvedTheme } = useTheme();
+  const { edgestore } = useEdgeStore();
+
+  const handleUpload = async (file: File) => {
+    const response = await edgestore.publicFiles.upload({
+      file,
+    });
+
+    return response.url;
+  };
+
   const editor: BlockNoteEditor = useBlockNote({
     editable,
     initialContent: initialContent
@@ -21,6 +32,7 @@ const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
     onEditorContentChange: (editor) => {
       onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
     },
+    uploadFile: handleUpload,
   });
   return (
     <div>
