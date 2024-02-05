0ae73a8b7903f959bf3f9335747e5095122fba27
jhshakil <shakil11271@gmail.com>
2024-02-06 01:27:24 +0600

    solving bg color problem

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -21,7 +21,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
     return redirect("/");
   }
   return (
-    <div className="h-screen flex dark:bd-[#1F1F1F]">
+    <div className="h-full flex dark:bg-[#1F1F1F]">
       <Navigation />
       <main className="flex-1 h-full overflow-y-auto">
         <SearchCommand />
